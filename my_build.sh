702eeb5e479d7e780852f1e449ae0a88c2875531
Ioanna Alifieraki <ioanna-maria.alifieraki@canonical.com>
2022-10-17 15:03:07 +0000

    Modify build and install scripts

--- my_build.sh
+++ my_build.sh
@@ -2,4 +2,4 @@
 
 ./configure --build=x86_64-linux-gnu --prefix=/usr --includedir=\${prefix}/include --mandir=\${prefix}/share/man --infodir=\${prefix}/share/info --sysconfdir=/etc --localstatedir=/var --disable-option-checking --disable-silent-rules --libdir=\${prefix}/lib/x86_64-linux-gnu --runstatedir=/run --disable-maintainer-mode --disable-dependency-tracking --enable-shared --libdir=/lib/x86_64-linux-gnu --enable-isadir=/lib/security --with-systemdunitdir=/usr/lib/systemd/system --disable-nis
 
-make -j8
+# make -j8
--- my_install.sh
+++ my_install.sh
@@ -4,6 +4,6 @@ find -type f -name "*.so" -print0 | xargs -I '{}' -0 cp '{}' temp
 
 mv temp/* /lib/x86_64-linux-gnu/security/
 
-mv ./libpamc/.libs/libpamc.so.0.82.1 /lib/x86_64-linux-gnu/
-mv ./libpam/.libs/libpam.so.0.85.1 /lib/x86_64-linux-gnu/
-mv ./libpam_misc/.libs/libpam_misc.so.0.82.1 /lib/x86_64-linux-gnu/
+cp ./libpamc/.libs/libpamc.so.0.82.1 /lib/x86_64-linux-gnu/
+cp ./libpam/.libs/libpam.so.0.85.1 /lib/x86_64-linux-gnu/
+cp ./libpam_misc/.libs/libpam_misc.so.0.82.1 /lib/x86_64-linux-gnu/
